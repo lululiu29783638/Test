857fd3620937d555759ace39dbea5bdd0211cb07
Lulu <lulu@Lulude-MacBook-Pro.local>
2019-01-09 19:19:46 +0800

    first commit

---
+++ Test/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Test
+//
+//  Created by Lulu  on 2019/1/9.
+//  Copyright © 2019 Lulu . All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -8,12 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
 
+    @IBAction func clickTextField(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textField.backgroundColor = UIColor.red
+        textField.addTarget(self, action: #selector(self.click(_:)), for: .editingChanged)
     }
+    
+    @IBAction func inputTextField(_ sender: Any) {
+        
+    }
+    
+    @objc func click(_ textField: UITextField) {
+        
+    }
+    
+
+    
+    
 
 
 }
